6135c784030928c0a19cc74aede797b252c2c7fb
Zywczas <zywczas@o2.pl>
2020-03-03 08:19:38 +0000

    w polowie robienia create user

--- app/src/main/java/com/example/gg2020/Services/AuthService.kt
+++ app/src/main/java/com/example/gg2020/Services/AuthService.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.gg2020.Utilities.URL_CREATE_USER
 import com.example.gg2020.Utilities.URL_LOGIN
 import com.example.gg2020.Utilities.URL_REGISTER
 import org.json.JSONException
@@ -25,22 +25,23 @@ object AuthService {
         jsonBody.put("password", password)
         val requestBody = jsonBody.toString()                                                //web request musi to podac jako byte array, wiec teraz zamieniamy na string a pozniej na byte array
 
-        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener{ response ->   //StringRequest - klasa z pakietu Volley
+        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener{ response ->   //StringRequest - klasa z pakietu Volley, object = anonymou inner class
             complete(true)
         }, Response.ErrorListener {error ->
-            Log.d("ERROR", "nie mozna stworzyc uzytkownika: $error")
+            Log.d("ERROR", "cannot register user: $error")
             complete(false)
         }) {                                                                                        //now we specify body content type
-            override fun getBodyContentType(): String {
-                return "application/json; charset=utf-8"
+            override fun getBodyContentType(): String {                                             //2 metody wymagane przez API, pobrane z JSONRequest
+                return "application/json; charset=utf-8"                                            //podajemy body content type
             }
 
-            override fun getBody(): ByteArray {
+            override fun getBody(): ByteArray {                                                     //podajemy body content
                 return requestBody.toByteArray()
             }
         }
         Volley.newRequestQueue(context).add(registerRequest)                                        //web request stworzony wiec teraz dodajemy go do kolejki
     }
+
 
     fun loginUser (context: Context, email: String, password: String, complete: (Boolean) -> Unit){
 
@@ -53,7 +54,7 @@ object AuthService {
             //this is where we parse json object
             try {
                 userEmail = response.getString("user")
-                authToken = response.getString("token")                               //ze zwroconego obiektu json chcemy pobrac wartosc dla key "token", wartosc jest typu String
+                authToken = response.getString("token")                               //ze zwroconego obiektu json pobieramy wartosc dla key "token", wartosc jest typu String
                 isLoggedIn = true
                 complete(true)
             } catch (e: JSONException){
@@ -62,7 +63,7 @@ object AuthService {
             }
 
         }, Response.ErrorListener { error ->
-            Log.d("ERROR", "nie mozna zalogowac uzytkownika: $error")
+            Log.d("ERROR", "cannot log in user: $error")
             complete(false)
         }){                                                                                        //now we specify body content type
             override fun getBodyContentType(): String {
@@ -75,6 +76,37 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(loginRequest)
+    }
+
+    fun createUser (context: Context, name: String, email: String, avatarName: String, avatarColor: String, complete: (Boolean) -> Unit){
+
+        val jsonBody = JSONObject()
+        jsonBody.put("name", name)                                                           //w tym kroku wysylamy dane nowego uzytkownika do API
+        jsonBody.put("email", email)                                                         //kolejnosc keys musi byc taka sama jak w API
+        jsonBody.put("avatarName", avatarName)
+        jsonBody.put("avatarColor", avatarColor)
+        val requestBody = jsonBody.toString()
+
+        val createRequest = object : JsonObjectRequest (Method.POST, URL_CREATE_USER, null, Response.Listener { responce ->
+
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "cannot add user: $error")
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {                                             //3 metody wymagane przez API, pobrane z JSONRequest
+                return "application/json; charset=utf-8"                                            //podajemy body content type
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {                                 //ta funkcja wysyla header do API w postaci Mapy<Key, Value>
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")                                   //nazewnictwo z header danej (naszej) API
+                return headers
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/gg2020/Services/UserDataService.kt
@@ -0,0 +1,10 @@
+package com.example.gg2020.Services
+
+object UserDataService {
+
+    var id = ""
+    var name = ""
+    var email = ""
+    var avatarColor = ""
+    var avatarName = ""
+}
