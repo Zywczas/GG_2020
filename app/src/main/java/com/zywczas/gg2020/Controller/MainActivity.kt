4e20303fc60a28c3ebc2b2ab7f2af3c353c387b0
Zywczas <zywczas@o2.pl>
2020-07-24 11:32:47 +0200

    deleted unnecessary layout files and added setupDrawer()

--- app/src/main/java/com/zywczas/gg2020/Controller/MainActivity.kt
+++ app/src/main/java/com/zywczas/gg2020/Controller/MainActivity.kt
@@ -20,6 +20,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -40,39 +41,16 @@ import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//todo dodac private
     val socket = IO.socket(SOCKET_URL)
     lateinit var channelAdapter : ArrayAdapter<Channel>
     lateinit var messageAdapter : MessageAdapter
     var selectedChannel: Channel? = null
 
-    private fun setupAdapters (){
-        channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-            MessageService.channels)
-        channel_list.adapter = channelAdapter
-
-        messageAdapter = MessageAdapter(this, MessageService.messages)
-        messageListView.adapter = messageAdapter
-        val layoutManager = LinearLayoutManager(this)
-        messageListView.layoutManager = layoutManager
-    }
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupDrawer()
 
         socket.connect()
         socket.on("channelCreated", onNewChannel)                                             //odbiera dane z API caly czas , jezeli Event wykryty to Emmitter Listener odbiera informacje
@@ -85,12 +63,31 @@ class MainActivity : AppCompatActivity() {
 
         channel_list.setOnItemClickListener { _, _, position, _ ->
             selectedChannel = MessageService.channels[position]
-            drawerLayout.closeDrawer(GravityCompat.START)                                           //closes drawer layout to the left (START)
+            drawer_layout.closeDrawer(GravityCompat.START)                                           //closes drawer layout to the left (START)
             updateWithChannel()
         }
 
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,   //odbieramy dane z CreateUserActivity & AuthService.findUser
             IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+//todo sprawdzic czy potrzebuje wsystkie stringi
+    private fun setupDrawer(){
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun setupAdapters (){
+        channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+            MessageService.channels)
+        channel_list.adapter = channelAdapter
+
+        messageAdapter = MessageAdapter(this, MessageService.messages)
+        messageListView.adapter = messageAdapter
+        val layoutManager = LinearLayoutManager(this)
+        messageListView.layoutManager = layoutManager
     }
 
     private val onNewChannel = Emitter.Listener {args ->                                            //przyjmuje z API array elementow typu ANY wiec musimy cast as String
@@ -223,13 +220,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    fun hideKeyboard(){
+    private fun hideKeyboard(){
         val inputManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager                    //bierzemy dostep do serwisu o nazwie INPUT..., czyli do klawiatury telefonu i odbieramy to jako klase InputMethodManager
         if (inputManager.isAcceptingText){
